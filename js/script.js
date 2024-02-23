5129df139f35fd122f42d482566bc97f14f4f89d
AlexF <fennecfr@gmail.com>
2024-02-23 22:29:04 +0100

    final commit graphic design is my passion

--- js/script.js
+++ js/script.js
@@ -2,7 +2,11 @@ const opciones = ["piedra", "papel", "tijera"];
 const botonesJugada = document.querySelectorAll(".boton-jugada");
 
 const tablaResultados = document.getElementById("resultados")
+let contadorJugador = 0
+let contadorMaquina = 0
+const contadorHTMLjugador = document.getElementById("contador-usuario")
 
+const contadorHTMLpc = document.getElementById("contador-ordenador")
 
 
 botonesJugada.forEach(function(elemento) {
@@ -23,12 +27,19 @@ botonesJugada.forEach(function(elemento) {
         }
         else if (eleccionJugador === "piedra" && eleccionPC === "tijera" || eleccionJugador === "papel" && eleccionPC === "piedra" || eleccionJugador === "tijera" && eleccionPC === "papel"){
             resultado = "Has ganado"
+            contadorJugador ++
+
         }
         else {
             resultado = "Has perdido"
+            contadorMaquina ++
+            
+
         }
         
-        tablaResultados.innerHTML = resultado + ". Has elegido " + eleccionJugador + " y la máquina ha elegido " + eleccionPC
+        tablaResultados.innerHTML = resultado + ". Has elegido " + eleccionJugador + " y la máquina ha elegido " + eleccionPC + "."
+        contadorHTMLjugador.innerText = "Tus puntos: " + contadorJugador
+        contadorHTMLpc.innerText = "Puntos de la máquina: " + contadorMaquina
     });
 });
 
